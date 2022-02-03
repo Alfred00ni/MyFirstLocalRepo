6364028075dd2f48a3e0164e2560f5669cedcb0a
Alfredo Bustamante <alfredobustamante@.mail.weber.edu>
2022-02-02 18:30:08 -0700

    AddedPrintExamples

--- Variables.py
+++ Variables.py
@@ -1,4 +1,15 @@
 # This is a comment 
-# variables in python 
+# Variables in python 
 # By: Alfredo
-#  CS 1030
+# CS 1030
+
+# Variables
+
+from distutils.command.build_scripts import first_line_re
+
+
+first_name = 'Alfredo'
+last_name = 'Bustamante'
+
+print (first_name, last_name)
+print ('Mr.' + first_name +' '+ last_name)
