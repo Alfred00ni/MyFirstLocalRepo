9fde7362dc3b90d1f0170c3408d54bfafdf6cca1
Alfredo Bustamante <alfredobustamante@.mail.weber.edu>
2022-02-02 18:35:09 -0700

    ChangedLastName

--- Variables.py
+++ Variables.py
@@ -9,7 +9,7 @@ from distutils.command.build_scripts import first_line_re
 
 
 first_name = 'Alfredo'
-last_name = 'Bustamante'
+last_name = 'Carrillo'
 
 print (first_name, last_name)
 print ('Mr.' + first_name +' '+ last_name)
